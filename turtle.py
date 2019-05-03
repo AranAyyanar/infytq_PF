a5aab291efe645682ca7c32f5510722dcbf884fd
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-03 15:20:37 +0530

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,19 @@
+import turtle              
+wn = turtle.Screen()      
+wn.setup(540,508)           
+
+alex = turtle.Turtle()      
+alex.shape("turtle")        
+alex.color('red')          
+
+alex.circle(50)
+alex.forward(100)
+alex.right(90)
+alex.backward(100)
+alex.left(90)
+alex.backward(200)
+alex.right(90)
+alex.forward(100)
+alex.right(90)
+alex.backward(200)
+alex.right(-90)
