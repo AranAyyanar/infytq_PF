e7b04f2f309276b9d8d46173a7752a72a787b26b
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-06 11:10:33 +0530

    Create loan_cal.py

---
+++ loan_cal.py
@@ -0,0 +1,16 @@
+def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_expected,customer_emi_expected):
+    eligible_loan_amount=0
+    bank_emi_expected=0
+    eligible_loan_amount=0
+    temp_num=account_number
+    count=0
+    while(temp_num>0):
+        remainder=temp_num%10
+        temp_num=temp_num//10
+        count=count+1
+    print(count)
+    if(count==4):
+        
+    
+    
+calculate_loan(1001,40000,250000,"Car",300000,30)
