18d6ca9bf5e4b46ba23c88bad3989742c64cd78a
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-06 11:51:11 +0530

    Update loan_cal.py

--- loan_cal.py
+++ loan_cal.py
@@ -1,3 +1,4 @@
+
 def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_expected,customer_emi_expected):
     eligible_loan_amount=0
     bank_emi_expected=0
@@ -8,9 +9,46 @@ def calculate_loan(account_number,salary,account_balance,loan_type,loan_amount_e
         remainder=temp_num%10
         temp_num=temp_num//10
         count=count+1
-    print(count)
-    if(count==4):
-        
-    
-    
+    if(count==4 and remainder==1):
+        if(account_balance>=100000):
+            if(loan_type=="Car" and salary>25000 and salary<50000):
+                   eligible_loan_amount=500000
+                   bank_emi_expected=36
+                   if(loan_amount_expected<=eligible_loan_amount and customer_emi_expected<=bank_emi_expected):
+                         print("Account number:", account_number)
+                         print("The customer can avail the amount of Rs.", eligible_loan_amount)
+                         print("Eligible EMIs :", bank_emi_expected)
+                         print("Requested loan amount:", loan_amount_expected)
+                         print("Requested EMI's:",customer_emi_expected)
+                   else:
+                       print("The customer is not eligible for the loan")
+            elif(loan_type=="House" and salary>50000 and salary<75000):
+                   eligible_loan_amount=6000000
+                   bank_emi_expected=60
+                   if(loan_amount_expected<=eligible_loan_amount and customer_emi_expected<=bank_emi_expected):
+                      print("Account number:", account_number)
+                      print("The customer can avail the amount of Rs.", eligible_loan_amount)
+                      print("Eligible EMIs :", bank_emi_expected)
+                      print("Requested loan amount:", loan_amount_expected)
+                      print("Requested EMI's:",customer_emi_expected)
+                   else:
+                        print("The customer is not eligible for the loan")
+            elif(loan_type=="Business"and salary>75000):
+                eligible_loan_amount=7500000
+                bank_emi_expected=84
+                if(loan_amount_expected<=eligible_loan_amount and customer_emi_expected<=bank_emi_expected):
+                     print("Account number:", account_number)
+                     print("The customer can avail the amount of Rs.", eligible_loan_amount)
+                     print("Eligible EMIs :", bank_emi_expected)
+                     print("Requested loan amount:", loan_amount_expected)
+                     print("Requested EMI's:",customer_emi_expected)
+                else:
+                       print("The customer is not eligible for the loan")
+            else:
+               print("Invalid loan type or salary")
+        else:
+            print("Insufficient account balance")
+    else:
+        print("Invalid account number")
+             
 calculate_loan(1001,40000,250000,"Car",300000,30)
