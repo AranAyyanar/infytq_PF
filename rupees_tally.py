3d109b0ad3b6dec58d6ffe45aab58a4b2bd317e9
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-06 09:02:44 +0530

    Update rupees_tally.py

--- rupees_tally.py
+++ rupees_tally.py
@@ -1,3 +1,10 @@
+
+def rupees(temp,no_of_one,one_needed,rupees_to_make):
+     for i in range(1,no_of_one+1):
+            temp=temp+1
+            if(temp<=rupees_to_make):
+               one_needed=one_needed+1
+     print("No. of One needed  :", one_needed)
 def make_amount(rupees_to_make,no_of_five,no_of_one):
     five_needed=0
     one_needed=0
@@ -11,14 +18,14 @@ def make_amount(rupees_to_make,no_of_five,no_of_one):
             if(sum<=rupees_to_make):
                     five_needed=five_needed+1
                     temp=sum
-        print('No. of Five needed :')
-        print(five_needed)
-        for i in range(1,no_of_one+1):
-            temp=temp+1
-            if(temp<=rupees_to_make):
-               one_needed=one_needed+1
-        print('No. of One needed :')
-        print(one_needed)
+        if((temp+no_of_one)>=rupees_to_make):
+             print("No. of Five needed :", five_needed)
+             rupees(temp,no_of_one,one_needed,rupees_to_make)
+        else:
+            print(-1)
+        
     else:
         print(-1)
-make_amount(21,4,2)
+    
+
+make_amount(93,19,2)
