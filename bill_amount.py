55201975a86c580a063f6dbc0054fd39940e64c8
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-06 10:49:36 +0530

    Update bill_amount.py

--- bill_amount.py
+++ bill_amount.py
@@ -1,19 +1,33 @@
 def calculate_bill_amount(food_type,quantity_ordered,distance_in_kms):
     bill_amount=0
-    if(food_type=="N"):
+    if(food_type=="N"and quantity_ordered>0and distance_in_kms>0):
         if(quantity_ordered>=1):
             bill_amount=150*quantity_ordered
-            for i in range(0,distance_in_kms+1):
-                if(i<=3):
+            for i in range(1,distance_in_kms+1):
+                if(i>=1 and i<=3):
                     bill_amount=bill_amount
                 elif(i>3 and i<=6):
-                    bill_amount=bill_amount+((i-3)+3)
+                    bill_amount=bill_amount+3
                 elif(i>6):
-                    bill_amount=bill_amount+((i-6)+6)
+                    bill_amount=bill_amount+6
                 else:
-                    bill_amount=-1
+                    pass
+    elif(food_type=="V"and quantity_ordered>0and distance_in_kms>0):
+        if(quantity_ordered>=1):
+            bill_amount=120*quantity_ordered
+            for i in range(1,distance_in_kms+1):
+                if(i>=1 and i<=3):
+                    bill_amount=bill_amount
+                elif(i>3 and i<=6):
+                    bill_amount=bill_amount+3
+                elif(i>6):
+                    bill_amount=bill_amount+6
+                else:
+                    pass
+    else:
+        bill_amount=-1
     return bill_amount
 
 
-bill_amount=calculate_bill_amount("N",2,7)
+bill_amount=calculate_bill_amount("N",1,7)
 print(bill_amount)
