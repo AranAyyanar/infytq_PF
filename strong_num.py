12bcaa39686d1baa31631769175026b17b81856a
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-13 13:26:15 +0530

    Update strong_num.py

--- strong_num.py
+++ strong_num.py
@@ -12,16 +12,19 @@ def find_strong_numbers(num_list):
     list1=[]
     for num in num_list:
         temp=num
-        while temp>0:
-            remainder=temp%10
-            sum1=sum1+factorial(remainder)
-            temp=temp//10
+        if num==0:
+            sum1=1
+        else:
+            while temp>0:
+               remainder=temp%10
+               sum1=sum1+factorial(remainder)
+               temp=temp//10
         if num==sum1:
             list1.append(num)
             sum1=0
         else:
-            sum1
+            sum1=0
     return list1
-num_list=[145,375,100,2,10]
+num_list=[0]
 strong_num_list=find_strong_numbers(num_list)
 print(strong_num_list)
