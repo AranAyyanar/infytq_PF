3a36e338046548d415698099febc645c68963cb0
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-13 16:03:57 +0530

    Create chocolates_received.py

---
+++ chocolates_received.py
@@ -0,0 +1,21 @@
+child_id=(10,20,30,40,50)
+chocolates_received=[12,5,3,4,6]
+def calculate_total_chocolates():
+    sum1=0
+    for total in chocolates_received:
+        sum1=sum1+total
+    return sum1
+def reward_child(child_id_rewarded,extra_chocolates):
+    if child_id_rewarded in child_id:
+        if extra_chocolates>1:
+            for i in range(0,len(child_id)):
+                if child_id[i]==child_id_rewarded:
+                    chocolates_received[i]=chocolates_received[i]+extra_chocolates
+                    print(chocolates_received)                   
+        else:
+            print("Extra chocolates is less than 1")
+    else:
+        print("Child id is invalid")
+
+print(calculate_total_chocolates())
+reward_child(20,2)
