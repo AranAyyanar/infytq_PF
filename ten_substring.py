bef8a386ba84a0e1f2c26737e22e691a70ba3821
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-15 06:54:48 +0000

    Update ten_substring.py

--- ten_substring.py
+++ ten_substring.py
@@ -1,26 +1,22 @@
-def find_ten_substring(num_str):
+ddef find_ten_substring(num_str):
     list1=[]
     list2=[]
+    list3=[]
+    join=''
     for i in num_str:
         list1.append(int(i))
-    for j in range(0,len(list1)-1):
-        if (list1[j]+list1[j+1])==10:
-            join=str(list1[j])+str(list1[j+1])
-            list2.append(join)
-    for j in range(0,len(list1)-2):
-        if (list1[j]+list1[j+1]+list1[j+2])==10:
-            join=str(list1[j])+str(list1[j+1])+str(list1[j+2])
-            list2.append(join)
-    for j in range(0,len(list1)-3):
-        if (list1[j]+list1[j+1]+list1[j+2]+list1[j+3])==10:
-            join=str(list1[j])+str(list1[j+1])+str(list1[j+2])+str(list1[j+3])
-            list2.append(join)
-    for j in range(0,len(list1)-4):
-        if (list1[j]+list1[j+1]+list1[j+2]+list1[j+3]+list1[j+4])==10:
-            join=str(list1[j])+str(list1[j+1])+str(list1[j+2])+str(list1[j+3])+str(list1[j+4])
-            list2.append(join)
-    return list2
-
+    for i in num_str:
+        list2.append(i)
+    for i in range(0,len(list1)):
+        for j in range(0,len(list1)-i):
+            sum1=sum(list1[i:(j+i)+1])
+            if sum1==10:
+                for f in range(i,(j+i)+1):
+                    join=join+list2[f]
+                list3.append(join)
+                join=''
+    return list3
+     
 num_str="2825302"
 print("The number is:",num_str)
 result_list=find_ten_substring(num_str)
