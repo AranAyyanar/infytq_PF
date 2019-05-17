08798fddfb11ee30c1472029a488850fd69ecdcf
AranAyyanar <47377904+AranAyyanar@users.noreply.github.com>
2019-05-17 09:38:02 +0530

    Create number_of_combination.py

---
+++ number_of_combination.py
@@ -0,0 +1,8 @@
+def find_number_of_combination(number_of_flavours):
+    total_combination=0
+    total_combination=2**number_of_flavours
+    
+    return total_combination
+
+number_of_combination=find_number_of_combination(6)
+print(number_of_combination)
